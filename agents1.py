a9372561fb76e86ad3c8037d51b8b61cc9a81f5a
Carsten Knoll <carsten.knoll@posteo.de>
2023-11-04 00:11:10 +0100

    agents1.py: add a number of persons from guest lists of two workshops (+memory)
    # Last 3 commits:
    
    # 9251cdf math1.py: make use of signature inheritance (R8, ..., R11) 
    # cc5e190 ct: remove obsolete reference to p.Sequence 
    # 9ae87dd Merge branch 'develop_carsten' into develop

--- agents1.py
+++ agents1.py
@@ -22,7 +22,7 @@ p.start_mod(__URI__)
 R7781 = p.create_relation(
     R1__has_label="has family name",
     R2__has_description="part of the full name of a person",
-    R33__has_corresponding_wikidata_entity="http://www.wikidata.org/entity/P734",
+    R33__has_corresponding_wikidata_entity="https://www.wikidata.org/wiki/Property:P734",
 )
 
 R7782 = p.create_relation(
@@ -32,7 +32,14 @@ R7782 = p.create_relation(
         "this relation is non-functional, i.e. a person can have multiple given names; order matters",
         "if given name is unknown, it is acceptable to use initials here",
     ],
-    R33__has_corresponding_wikidata_entity="http://www.wikidata.org/entity/P735",
+    R33__has_corresponding_wikidata_entity="https://www.wikidata.org/wiki/Property:P735",
+)
+
+R3474 = p.create_relation(
+    R1__has_label="has ORCID",
+    R2__has_description="specifies the orcid of a researcher",
+    R18__has_usage_hint="This can be used if no wikidata entry is yet available",
+    R33__has_corresponding_wikidata_entity="https://www.wikidata.org/wiki/Property:P496",
 )
 
 
@@ -44,7 +51,7 @@ I7435 = p.create_item(
 )
 
 
-def create_person(given_name: str, family_name: str, r2: str, r33=None):
+def create_person(given_name: str, family_name: str, r2: str, r33=None, r3474=None):
     """
     This is a convenience function that simplifies the creation of items for humans
     """
@@ -61,7 +68,12 @@ def create_person(given_name: str, family_name: str, r2: str, r33=None):
     )
 
     if r33:
+        assert isinstance(r33, str)
         item.set_relation(p.R33["has corresponding wikidata entity"], r33)
+    if r3474:
+        assert isinstance(r3474, str)
+        item.set_relation(R3474["has ORCID"], r3474)
+
     return item
 
 I2746 = create_person("Rudolf", "Kalman", "electrical engineer and mathematician")
@@ -139,7 +151,85 @@ I1257 = create_person("Joseph Pierre", "LaSalle", "mathematician", r33="https://
 I8430 = create_person("Eduardo Daniel", "Sontag", "mathematician", r33="https://www.wikidata.org/wiki/Q3709600")
 
 I7906 = create_person("Rudolf", "Lipschitz", "mathematician", r33="https://www.wikidata.org/wiki/Q77322")
+
 I4853 = create_person("Sophus", "Lie", "mathematician", r33="https://www.wikidata.org/wiki/Q30769")
+
+I7115 = create_person("Michel", "Fliess", "computer scientist", r33="https://www.wikidata.org/wiki/Q3309635")
+
+I3474 = create_person("Joachim", "Rudolph", "engineer", r3474="https://orcid.org/0000-0001-8402-2458")
+
+I8885 = create_person("Frank", "Allgöwer", "engineer", r33="https://www.wikidata.org/wiki/Q1238632")
+
+I1639 = create_person("Frank", "Woittennek", "engineer", r3474="https://orcid.org/0000-0001-8268-9582")
+
+I9437 = create_person("Albrecht", "Gensior", "engineer", r3474="https://orcid.org/0000-0002-4276-6819")
+
+I1635 = create_person("Johannes", "Reuter", "engineer", r3474="https://orcid.org/0000-0003-2069-5682")
+
+I5667 = create_person("Kathrin", "Flaßkamp", "engineer", r3474="https://orcid.org/0000-0001-5983-1907")
+
+I8521 = create_person("Jan", "Winkler", "engineer", r3474="https://orcid.org/0000-0001-9499-0030")
+
+I3973 = create_person("Kurt", "Schlacher", "engineer")
+
+I7642 = create_person("Hugues", "Mounier", "engineer", r3474="https://orcid.org/0000-0002-7069-3119")
+
+I3749 = create_person("Greta", "Zimmer", "engineer")
+
+I3520 = create_person("Jörg", "Raisch", "engineer", r33="https://www.wikidata.org/wiki/Q107287703")
+
+I1690 = create_person("Nicole", "Gehring", "engineer", r33="https://www.wikidata.org/wiki/Q60780539")
+
+I3238 = create_person("Matthias", "Franke", "engineer", r33="https://www.wikidata.org/wiki/Q57423172")
+
+I9404 = create_person("Kurt", "Reinschke", "engineer", r33="https://www.wikidata.org/wiki/Q106238929")
+
+I2377 = create_person("Andreas", "Kugi", "engineer", r33="https://www.wikidata.org/wiki/Q102409925")
+
+I6176 = create_person("Alberto", "Isidori", "control theorist", r33="https://www.wikidata.org/wiki/Q3608377")
+
+I8526 = create_person("Jens", "Saak", "researcher", r33="https://www.wikidata.org/wiki/Q87670704")
+
+I4329 = create_person("Jürgen", "Adamy", "researcher", r33="https://www.wikidata.org/wiki/Q99739075")
+
+I7718 = create_person("Joachim", "Deutscher", "researcher", r3474="https://orcid.org/0000-0001-6596-6066")
+
+I4722 = create_person("Knut", "Graichen", "researcher", r33="https://www.wikidata.org/wiki/Q102409924")
+
+I6846 = create_person("Thomas", "Meurer", "researcher", r33="https://www.wikidata.org/wiki/Q60576394")
+
+I7912 = create_person("Stefan", "Ecklebe", "engineer", r3474="https://orcid.org/0000-0002-4911-1233")
+
+I3867 = create_person("Timm", "Faulwasser", "researcher", r33="https://www.wikidata.org/wiki/Q56426088")
+
+I6334 = create_person("Rolf", "Findeisen", "researcher", r33="https://www.wikidata.org/wiki/Q56426090")
+
+I4666 = create_person("Daniel", "Gerbet", "engineer", r3474="https://orcid.org/0000-0002-8527-8727")
+
+I6725 = create_person("Claus", "Hillermeier", "engineer", r33="https://www.wikidata.org/wiki/Q112355168")
+
+I9170 = create_person("Peter", "Hippe", "engineer")
+
+I7086 = create_person("Abdurrahman", "Irscheid", "engineer", r3474="https://orcid.org/0000-0002-5913-2468")
+
+I4786 = create_person("Paul", "Kotyczka", "engineer", r3474="https://orcid.org/0000-0002-6669-6368")
+
+I3764 = create_person("Boris", "Lohmann", "engineer", r33="https://www.wikidata.org/wiki/Q59543808")
+
+# note: unclear whether orcid exists (too many people with this name)
+I8739 = create_person("Victor", "Lopez", "engineer")
+
+# note: unclear whether orcid exists (too many people with this name)
+I9992 = create_person("Matthias", "Müller", "engineer")
+
+I4854 = create_person("Amine", "Othmane", "engineer")
+
+I3431 = create_person("Karl", "Worthmann", "mathematician")
+
+I1361 = create_person("Jens", "Wurm", "engineer", r3474="https://orcid.org/0000-0002-6760-7160")
+
+
+
 
 
 I6591 = p.create_item(
@@ -271,31 +361,6 @@ p.end_mod()
 """
 key reservoir created with: `pyerk -l agents1.py ag -nk 100`
 
-I1848
-I7115      R7115
-I3474      R3474
-I1639      R1639
-I8885      R8885
-I9437      R9437
-I1635      R1635
-I5667      R5667
-I8521      R8521
-I3973      R3973
-I7642      R7642
-I3749      R3749
-I3520      R3520
-I1690      R1690
-I3238      R3238
-I9404      R9404
-I2377      R2377
-I6176      R6176
-I6725      R6725
-I9170      R9170
-I7086      R7086
-I4786      R4786
-I3764      R3764
-I8739      R8739
-I9992      R9992
 I3854      R3854
 I7934      R7934
 I6514      R6514
@@ -307,18 +372,7 @@ I6589      R6589
 I7064      R7064
 I6656      R6656
 I1928      R1928
-I1361      R1361
-I3431      R3431
-I4854      R4854
-I4666      R4666
-I6334      R6334
-I3867      R3867
-I7912      R7912
-I6846      R6846
-I4722      R4722
-I7718      R7718
-I4329      R4329
-I8526      R8526
+
 I6009      R6009
 I3563      R3563
 I8388      R8388
